72a26eeb3093134398fba9b17bd9789731e81876
Mortar1on <mortarion018@gmail.com>
2020-03-28 17:59:53 +0200

    Guess number homework

---
+++ lesson1/GuessNumber.java
@@ -0,0 +1,34 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+
+        int value = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        while (value != 35) {
+            System.out.println("type your number");
+
+            if (scanner.hasNextInt()) {
+                value = scanner.nextInt();
+                if (value == 35) {
+                    System.out.println("Got it! Your number is " + value);
+                } else if (value <= 29) {
+                    System.out.println("your number is less");
+                } else if (value <= 30 || value <= 40) {
+                    System.out.println("you are close");
+                } else if (value <= 41 || value <= 100) {
+                    System.out.println("your number is greater");
+                } else if (value >= 101) {
+                    System.out.println("you need to guess a number from 0 to 100");
+                }
+            } else {
+                scanner.nextLine();
+            }
+        }
+
+
+    }
+}
---
+++ lesson1/GuessNumber2.java
@@ -0,0 +1,33 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class GuessNumber2 {
+
+    public static void main(String[] args) {
+        int a = (int) (Math.random() * 101);
+        int value = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        while (value != a) {
+            System.out.println("type your number");
+
+            if (scanner.hasNextInt()) {
+                value = scanner.nextInt();
+                if (value == a) {
+                    System.out.println("Got it! Your number is " + a);
+                } else if (value < a - 5) {
+                    System.out.println("your number is less");
+                } else if (value < a + 6) {
+                    System.out.println("you are close");
+                } else if (value > a && value <= 100) {
+                    System.out.println("your number is greater");
+                } else if (value >= 101) {
+                    System.out.println("you need to guess a number from 0 to 100");
+                }
+            } else {
+                scanner.nextLine();
+            }
+        }
+    }
+}
