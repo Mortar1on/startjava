5048a7392ca4aac3bf14f90612d7b5bb7f2f53f1
Mortar1on <mortarion018@gmail.com>
2020-03-28 13:51:54 +0200

    ConditionalStatement homework

---
+++ lesson0/ConditionalStatement.java
@@ -0,0 +1,37 @@
+package lesson0;
+
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int age = 25;
+        boolean isMale = true;
+        double height = 1.90;
+        char firstNameLetter = 'M';
+
+        if (age > 20) {
+            System.out.println("Вы старше 20");
+        }
+
+        if (isMale) {
+            System.out.println("Вы мужчина");
+        }
+
+        if (!isMale) {
+            System.out.println("Вы женщина");
+        }
+
+        if (height < 1.80) {
+            System.out.println("Вы не подходите по росту");
+        } else {
+            System.out.println("Добро пожаловать!");
+        }
+
+        if (firstNameLetter == 'M') {
+            System.out.println("Ваше имя начинается на M");
+        } else if (firstNameLetter == 'I') {
+            System.out.println("Ваше имя начинается на I");
+        } else {
+            System.out.println("Ваше имя начинается на другую букву");
+        }
+
+    }
+}
