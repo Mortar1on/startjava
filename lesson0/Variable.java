3e6f2df694661750a8be01561736706a1092065c
Mortar1on <mortarion018@gmail.com>
2020-03-28 13:50:23 +0200

    Variable homework

---
+++ lesson0/Variable.java
@@ -0,0 +1,23 @@
+package lesson0;
+
+public class Variable {
+    public static void main(String[] args) {
+        byte core = 8;
+        short ram = 16;
+        int gpu = 8;
+        long ssdMemory = 512L;
+        double cpuFrequency = 5.1;
+        float ramSupplyVoltage = 1.2F;
+        char ch1 = 'Y';
+        boolean isQuarantine = true;
+
+        System.out.println(core);
+        System.out.println(ram);
+        System.out.println(gpu);
+        System.out.println(ssdMemory);
+        System.out.println(cpuFrequency);
+        System.out.println(ramSupplyVoltage);
+        System.out.println(ch1);
+        System.out.println(isQuarantine);
+    }
+}
