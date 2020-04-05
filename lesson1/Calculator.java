13cadced607872c70c86391024a100392ae7801f
Mortar1on <mortarion018@gmail.com>
2020-04-05 18:16:32 +0300

    Calculator homework

---
+++ lesson1/Calculator.java
@@ -0,0 +1,80 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class Calculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int number1 = getInt();
+        int number2 = getInt();
+        char operation = getOperation();
+        int result = calculations(number1, number2, operation);
+        System.out.println("Result:" + result);
+
+    }
+
+    private static int getInt() {
+        System.out.println("Enter a number:");
+        int number;
+        if (scanner.hasNextInt()) {
+            number = scanner.nextInt();
+        } else {
+            System.out.println("Error. Please enter a number");
+            scanner.next();
+            number = getInt();
+        }
+        return number;
+    }
+
+    private static char getOperation() {
+        System.out.println("Enter operation");
+        char operation;
+        if (scanner.hasNext()) {
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Error. Please enter an operation");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    private static int power(int a, int b) {
+        int result = 1;
+        for (int i = 1; i <= b; i++) {
+            result = result * a;
+        }
+        return result;
+    }
+
+    private static int calculations(int number1, int number2, char operation) {
+        int result;
+        switch (operation) {
+            case '+':
+                result = number1 + number2;
+                break;
+            case '-':
+                result = number1 - number2;
+                break;
+            case '*':
+                result = number1 * number2;
+                break;
+            case '/':
+                result = number1 / number2;
+                break;
+            case '^':
+                result = power(number1, number2);
+                break;
+            case '%':
+                result = number1 % number2;
+                break;
+            default:
+                System.out.println("Wrong operation. Please, retype");
+                result = calculations(number1, number2, getOperation());
+        }
+        return result;
+    }
+}
+
+
