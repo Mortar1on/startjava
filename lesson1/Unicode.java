73d87751daea54267b56a47ee72f5178d26c2eeb
Mortar1on <mortarion018@gmail.com>
2020-04-05 20:13:28 +0300

    Unicode homework

---
+++ lesson1/Unicode.java
@@ -0,0 +1,9 @@
+package lesson1;
+
+public class Unicode {
+    public static void main(String[] args) {
+        for (char c = 9398; c <= 10798; c++) {
+            System.out.println(c);
+        }
+    }
+}
