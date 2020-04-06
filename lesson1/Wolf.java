e62931f689021e65309236a3542f7b659c3d9bc3
Mortar1on <mortarion018@gmail.com>
2020-04-06 21:01:38 +0300

    Unicode homework

---
+++ lesson1/Wolf.java
@@ -0,0 +1,25 @@
+package lesson1;
+
+public class Wolf {
+    String gender = "male";
+    String nickname = "fang";
+    String color = "white";
+    double weight = 45.3;
+    int age = 3;
+
+    void walk() {
+        System.out.println("The wolf is walking");
+    }
+
+    void sit() {
+        System.out.println("The wolf is sitting");
+    }
+
+    void run() {
+        System.out.println("The wolf is running");
+    }
+
+    void howl() {
+        System.out.println("Wolf howls at the moon");
+    }
+}
