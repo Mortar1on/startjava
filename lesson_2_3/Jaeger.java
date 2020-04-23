624929d24f39f697f50c97e92d49d43c55f35c20
Mortar1on <mortarion018@gmail.com>
2020-04-23 17:12:12 +0300

    Some changes

---
+++ lesson_2_3/Jaeger.java
@@ -0,0 +1,105 @@
+package lesson_2_3;
+
+public class Jaeger {
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
+
+    public Jaeger(String modelName, String mark, String origin,
+                  float height, float weight,
+                  int speed, int strength, int armor) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+        this.strength = this.strength;
+        this.armor = armor;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public String getModelName() {
+        return this.modelName;
+    }
+
+    public String getMark() {
+        return this.mark;
+    }
+
+    public String getOrigin() {
+        return this.origin;
+    }
+
+    public float getHeight() {
+        return this.height;
+    }
+
+    public float getWeight() {
+        return this.weight;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public int getStrength() {
+        return this.strength;
+    }
+
+    public int getArmor() {
+        return this.armor;
+    }
+
+    boolean drift() {
+        return true;
+    }
+
+    void move() {
+
+    }
+
+    String scanKaiju() {
+        return "scanKaiju";
+    }
+
+    void useVortexCannon() {
+
+    }
+}
---
+++ lesson_2_3/JaegerTest.java
@@ -0,0 +1,33 @@
+package lesson_2_3;
+
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger robot1 = new Jaeger("Bracer Phoenix", "Mark-5", "USA",
+                70.7f, 2.1f,
+                3, 8, 9);
+
+
+        Jaeger robot2 = new Jaeger("Gipsy Danger", "Mark-3", "USA",
+                79.25f, 1.98f,
+                7, 8, 6);
+
+
+        System.out.println(robot1.getModelName());
+        System.out.println(robot1.getMark());
+        System.out.println(robot1.getOrigin());
+        System.out.println(robot1.getHeight());
+        System.out.println(robot1.getWeight());
+        System.out.println(robot1.getSpeed());
+        System.out.println(robot1.getStrength());
+        System.out.println(robot1.getArmor());
+
+        System.out.println(robot2.getModelName());
+        System.out.println(robot2.getMark());
+        System.out.println(robot2.getOrigin());
+        System.out.println(robot2.getHeight());
+        System.out.println(robot2.getWeight());
+        System.out.println(robot2.getSpeed());
+        System.out.println(robot2.getStrength());
+        System.out.println(robot2.getArmor());
+    }
+}
